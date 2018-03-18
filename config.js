a11b97f1bb47d2d5a93f1518dc61f55af31d6d9f
m41highway@gamil.com <thaib@Macintosh.local>
2018-03-18 10:39:45 +0800

    Hide API key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
  firebase: {
-  apiKey: "AIzaSyDCf9uEKSNUzIwtNnI8W3m7RlMNC6NlJPY",
+  apiKey: "xxxx",
   authDomain: "restaurants-1b2e4.firebaseapp.com",
   databaseURL: "https://restaurants-1b2e4.firebaseio.com",
   projectId: "restaurants-1b2e4",
